b060207edf26b1c61b9013ba3898a4a9fe8118ab
Jordan Lee <jordan@jordanlee.net>
2021-11-17 14:05:25 +1100

    Initial commit

---
+++ src/JL.Test.Package/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JL.Test.Package
+{
+    public static class StringExtensions
+    {
+        public static void PrintXTimes(this string x, int times)
+        {
+            if (times <= 0)
+                throw new ArgumentOutOfRangeException(nameof(times), "Argument must be greater than 0");
+
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
